c74266a89850c58403f982488cd9d8f93135da15
OmarAbseh <omar_absah@icloud.com>
2025-05-18 20:35:47 +0200

    buttons and navigation work

--- app/src/main/java/com/example/cloudnovamobile/MainActivity.java
+++ app/src/main/java/com/example/cloudnovamobile/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.cloudnovamobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,25 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
         });
+
+        Button addTaskBtn = findViewById(R.id.btn_add_task);
+        Button viewTasksBtn = findViewById(R.id.btn_view_tasks);
+        Button settingsBtn = findViewById(R.id.btn_settings);
+
+        addTaskBtn.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, AddTaskActivity.class));
+        });
+
+        viewTasksBtn.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, TaskDetailActivity.class));
+        });
+
+        settingsBtn.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+        });
+
     }
 }
